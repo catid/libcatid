4f386f79a49cab7f68236b7c4714602cc5518e2d
mrcatid <mrcatid@204b7394-75af-11de-9510-cf8a13bd668e>
2009-08-11 05:53:36 +0000

    Implement Fortuna for Linux

--- src/crypt/rand/EntropyLinux.cpp
+++ src/crypt/rand/EntropyLinux.cpp
@@ -22,4 +22,219 @@ using namespace cat;
 
 #if defined(CAT_OS_LINUX)
 
+#include <cat/time/Clock.hpp>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+void FortunaFactory::EntropyCollectionThread()
+{
+    // Assume ~16 bits of entropy per fast poll, so it takes 16 fast polls to get 256 bits of entropy
+    // This means there will be 4 slow polls in pool 0 for each reseed, which is 256 bits from CryptoAPI
+    const int POOL0_RESEED_RATE = 16;
+
+    // Milliseconds between fast polls
+    // Indicates 51.2 seconds between reseeds
+    const int COLLECTION_PERIOD = 100;
+
+    int fast_pool = 0, slow_pool = 0, pool0_entropy = 0;
+
+    // Loop while the flag is set
+    while (thread_running)
+    {
+        Clock::sleep(COLLECTION_PERIOD);
+
+        // Poll fast entropy sources once every COLLECTION_PERIOD
+        PollFastEntropySources(fast_pool);
+
+        // Poll slow entropy sources 4 times slower
+        if ((fast_pool & 3) == 0)
+        {
+            PollSlowEntropySources(slow_pool);
+
+            // Keep track of entropy in pool 0 and reseed when it is ready
+            if (fast_pool == 0 && ++pool0_entropy >= POOL0_RESEED_RATE)
+            {
+                FortunaFactory::ii->Reseed();
+                pool0_entropy = 0;
+            }
+        }
+
+        slow_pool = (slow_pool + 1) % 32;
+        fast_pool = (fast_pool + 1) % 32;
+    }
+}
+
+void *FortunaFactory::EntropyCollectionThreadWrapper(void *vfactory)
+{
+    FortunaFactory *factory = (FortunaFactory *)vfactory;
+
+    factory->EntropyCollectionThread();
+
+    return 0;
+}
+
+
+bool FortunaFactory::InitializeEntropySources()
+{
+    urandom_fd = -1;
+
+    // Fire poll for entropy all goes into pool 0
+    PollInvariantSources(0);
+    PollSlowEntropySources(0);
+    PollFastEntropySources(0);
+
+    thread_running = true;
+    if (pthread_create(&pthread_handle, 0, &FortunaFactory::EntropyCollectionThreadWrapper, (void*)this))
+    {
+        thread_running = false;
+        return false;
+    }
+
+    urandom_fd = open("/dev/urandom", O_RDONLY);
+
+    return true;
+}
+
+void FortunaFactory::ShutdownEntropySources()
+{
+    if (thread_running)
+    {
+        thread_running = false;
+        pthread_join(pthread_handle, 0);
+    }
+
+    if (urandom_fd >= 0)
+        close(urandom_fd);
+}
+
+void FortunaFactory::PollInvariantSources(int pool_index)
+{
+    Skein &pool = Pool[pool_index];
+
+    struct {
+        u32 cycles_start;
+        u8 system_prng[32];
+        u32 pid;
+        u32 cycles_end;
+    } Sources;
+
+    // Cycles at the start
+    Sources.cycles_start = Clock::cycles();
+
+    // /dev/urandom large request
+    if (urandom_fd >= 0)
+        read(urandom_fd, Sources.system_prng, sizeof(Sources.system_prng));
+
+    // pid
+    Sources.pid = (u32)getpid();
+
+    // Cycles at the end
+    Sources.cycles_end = Clock::cycles();
+
+    pool.Crunch(&Sources, sizeof(Sources));
+}
+
+static void PollVMStat(Skein &pool)
+{
+    const char *PATH = "vmstat -s";
+
+    fd_set fds;
+    FD_ZERO(&fds);
+
+    if (!access(PATH, F_OK)) return;
+
+    FILE *fp = popen(PATH, "r");
+    if (!fp) return;
+
+    int fd = fileno(fp);
+    if (fd < 0) return;
+
+    FD_SET(fd, &fds);
+
+    timeval tv;
+    tv.tv_sec = 5;
+    tv.tv_usec = 0;
+
+    int r;
+    while ((r = select(1 + fd, &fds, 0, 0, &tv)))
+    {
+        if (r == -1 || !FD_ISSET(fd, &fds)) break;
+
+        static u8 buffer[4096];
+        int count = read(fd, buffer, sizeof(buffer));
+
+        if (count > 0) pool.Crunch(buffer, count);
+        else break;
+    }
+
+    pclose(fp);
+    FD_CLR(fd, &fds);
+}
+
+void FortunaFactory::PollSlowEntropySources(int pool_index)
+{
+    Skein &pool = Pool[pool_index];
+
+    struct {
+        u32 cycles_start;
+        u8 system_prng[8];
+        double this_request;
+        double request_diff;
+        u32 cycles_end;
+    } Sources;
+
+    // Cycles at the start
+    Sources.cycles_start = Clock::cycles();
+
+    // /dev/urandom small request
+    if (urandom_fd >= 0)
+        read(urandom_fd, Sources.system_prng, sizeof(Sources.system_prng));
+
+    // Poll vmstat -s
+    PollVMStat(pool);
+
+    // Poll time in microseconds
+    Sources.this_request = Clock::usec();
+
+    // Time since last poll in microseconds
+    static double last_request = 0;
+    Sources.request_diff = Sources.this_request - last_request;
+    last_request = Sources.this_request;
+
+    // Cycles at the end
+    Sources.cycles_end = Clock::cycles();
+
+    pool.Crunch(&Sources, sizeof(Sources));
+}
+
+void FortunaFactory::PollFastEntropySources(int pool_index)
+{
+    Skein &pool = Pool[pool_index];
+
+    struct {
+        u32 cycles_start;
+        double this_request;
+        double request_diff;
+        u32 cycles_end;
+    } Sources;
+
+    // Cycles at the start
+    Sources.cycles_start = Clock::cycles();
+
+    // Poll time in microseconds
+    Sources.this_request = Clock::usec();
+
+    // Time since last poll in microseconds
+    static double last_request = 0;
+    Sources.request_diff = Sources.this_request - last_request;
+    last_request = Sources.this_request;
+
+    // Cycles at the end
+    Sources.cycles_end = Clock::cycles();
+
+    pool.Crunch(&Sources, sizeof(Sources));
+}
+
 #endif
