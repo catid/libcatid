4bd72e1e72d79df9c3d1666064f7ef8b30042a8a
mrcatid <mrcatid@204b7394-75af-11de-9510-cf8a13bd668e>
2009-11-22 15:49:50 +0000

    Switch to BSD-style license

--- src/crypt/hash/HMAC_MD5.cpp
+++ src/crypt/hash/HMAC_MD5.cpp
@@ -1,20 +1,29 @@
 /*
-    Copyright 2009 Christopher A. Taylor
+	Copyright (c) 2009 Christopher A. Taylor.  All rights reserved.
 
-    This file is part of LibCat.
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
 
-    LibCat is free software: you can redistribute it and/or modify
-    it under the terms of the Lesser GNU General Public License as
-    published by the Free Software Foundation, either version 3 of
-    the License, or (at your option) any later version.
+	* Redistributions of source code must retain the above copyright notice,
+	  this list of conditions and the following disclaimer.
+	* Redistributions in binary form must reproduce the above copyright notice,
+	  this list of conditions and the following disclaimer in the documentation
+	  and/or other materials provided with the distribution.
+	* Neither the name of LibCat nor the names of its contributors may be used
+	  to endorse or promote products derived from this software without
+	  specific prior written permission.
 
-    LibCat is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    Lesser GNU General Public License for more details.
-
-    You should have received a copy of the Lesser GNU General Public
-    License along with LibCat.  If not, see <http://www.gnu.org/licenses/>.
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+	ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+	LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+	CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+	POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include <cat/crypt/hash/HMAC_MD5.hpp>
@@ -330,3 +339,32 @@ void HMAC_MD5::Generate(void *out, int bytes)
 
     memcpy(out, State, bytes);
 }
+
+#undef F
+#undef G
+#undef H
+#undef I
+
+#undef ROUND
+
+#undef FF
+#undef GG
+#undef HH
+#undef II
+
+#undef S11
+#undef S12
+#undef S13
+#undef S14
+#undef S21
+#undef S22
+#undef S23
+#undef S24
+#undef S31
+#undef S32
+#undef S33
+#undef S34
+#undef S41
+#undef S42
+#undef S43
+#undef S44
