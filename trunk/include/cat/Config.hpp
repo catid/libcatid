c2a012aa89c4213bb8256add3a80329bcebbdf90
mrcatid@gmail.com <mrcatid@gmail.com@204b7394-75af-11de-9510-cf8a13bd668e>
2011-02-16 20:31:44 +0000

    Fix a memory leak in fragmented messages

--- trunk/include/cat/Config.hpp
+++ trunk/include/cat/Config.hpp
@@ -53,6 +53,9 @@ namespace cat {
 // Adjust if your architecture uses larger than 128-byte cache line
 #define CAT_DEFAULT_CACHE_LINE_SIZE 128
 
+// Enable leak debug mode of the common runtime heap allocator
+#define CAT_DEBUG_LEAKS
+
 
 } // namespace cat
 
