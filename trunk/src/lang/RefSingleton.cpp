713d9866d470f937a0866a772c9009a8c65284d8
mrcatid@gmail.com <mrcatid@gmail.com@204b7394-75af-11de-9510-cf8a13bd668e>
2011-08-20 16:21:14 +0000

    Woot!  RefSingletons are live!

--- trunk/src/lang/RefSingleton.cpp
+++ trunk/src/lang/RefSingleton.cpp
@@ -29,12 +29,92 @@
 #include <cat/lang/RefSingleton.hpp>
 using namespace cat;
 
+
+//// Mutex
+
 static Mutex m_ref_singleton_mutex;
 
 Mutex &cat::GetRefSingletonMutex()
 {
 	return m_ref_singleton_mutex;
 }
+
+
+//// RefSingletonBase
+
+RefSingletonBase::RefSingletonBase()
+{
+	_refs_count = 0;
+}
+
+void RefSingletonBase::AddRefSingletonReference(u32 *ref_counter)
+{
+	u32 ii = _refs_count++;
+
+	if (ii < REFS_PREALLOC)
+	{
+		_refs_prealloc[ii] = ref_counter;
+	}
+	else
+	{
+		ii -= REFS_PREALLOC;
+
+		// Reallocate every time ii has under 2 bits set
+		if (ii < REFS_PREALLOC)
+		{
+			if (ii == 0)
+			{
+				_refs_extended = new u32*[REFS_PREALLOC];
+				if (!_refs_extended) { --_refs_count; return; }
+			}
+		}
+		else if (!CAT_AT_LEAST_2_BITS(ii))
+		{
+			u32 **new_extended = new u32*[ii + 1];
+			if (!new_extended) { --_refs_count; return; }
+
+			// If old data exists,
+			if (_refs_extended)
+			{
+				memcpy(new_extended, _refs_extended, sizeof(u32*) * ii);
+				delete []_refs_extended;
+			}
+
+			_refs_extended = new_extended;
+		}
+
+		_refs_extended[ii] = ref_counter;
+	}
+}
+
+void RefSingletonBase::ReleaseRefs()
+{
+	int count = _refs_count;
+
+	for (int ii = 0; ii < REFS_PREALLOC && ii < count; ++ii)
+	{
+		u32 *cnt = _refs_prealloc[ii];
+
+		*cnt = *cnt - 1;
+	}
+
+	int extended_count = count - REFS_PREALLOC;
+
+	if (extended_count > 0 && _refs_extended)
+	{
+		for (int ii = 0; ii < extended_count; ++ii)
+		{
+			u32 *cnt = _refs_extended[ii];
+
+			*cnt = *cnt - 1;
+		}
+
+		delete []_refs_extended;
+	}
+}
+
+
+//// RefSingletons
 
 void RefSingletons::OnExit()
 {
@@ -51,5 +131,37 @@ void RefSingletons::OnInitialize()
 
 void RefSingletons::OnFinalize()
 {
-	// TODO
+	// While there are still active singletons,
+	while (!_active_list.empty())
+	{
+		bool locked = true;
+
+		// For each active singleton,
+		for (iter ii = _active_list.head(); ii; ++ii)
+		{
+			u32 *ref_count = ii->GetRefCount();
+
+			// If reference count reaches zero,
+			if (*ref_count == 0)
+			{
+				ii->OnFinalize();
+				ii->ReleaseRefs();
+
+				_active_list.EraseMaintainIteration(ii);
+
+				locked = false;
+			}
+		}
+
+		// If locked up due to dangling references,
+		if (locked)
+		{
+			CAT_FATAL("RefSingleton") << "Unable to gracefully finalize all the RefSingletons due to dangling references.  Forcing the rest...";
+
+			for (iter ii = _active_list.head(); ii; ++ii)
+				ii->OnFinalize();
+
+			break;
+		}
+	}
 }
