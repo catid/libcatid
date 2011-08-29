ffa818ba18d05abab2642dfc339938ce4a57d90a
mrcatid@gmail.com <mrcatid@gmail.com@204b7394-75af-11de-9510-cf8a13bd668e>
2011-08-29 18:49:18 +0000

    Splitting out settings file parser into a new Ragdoll file format

---
+++ trunk/include/cat/io/RagdollFile.hpp
@@ -0,0 +1,312 @@
+/*
+	Copyright (c) 2009-2011 Christopher A. Taylor.  All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+
+	* Redistributions of source code must retain the above copyright notice,
+	  this list of conditions and the following disclaimer.
+	* Redistributions in binary form must reproduce the above copyright notice,
+	  this list of conditions and the following disclaimer in the documentation
+	  and/or other materials provided with the distribution.
+	* Neither the name of LibCat nor the names of its contributors may be used
+	  to endorse or promote products derived from this software without
+	  specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+	ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+	LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+	CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+	POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef CAT_RAGDOLL_FILE_HPP
+#define CAT_RAGDOLL_FILE_HPP
+
+#include <cat/lang/LinkedLists.hpp>
+#include <cat/lang/Strings.hpp>
+#include <string>
+
+/*
+	Ragdoll file format:
+
+	A human-readable/writable hierarchical key-value data store via text files.
+
+	I felt the itch to write a new settings file format for my online game code.
+	The goals are hierarchical key-value pairs, increased performance, smaller
+	file sizes, and clean and simple look&feel.  I'll compare it to XML.
+
+	Currently the standard for interpreted text data files is XML.
+	XML has a few nice properties:
+
+		+ XSD files allow for verification of an XML according to a template.
+		+ Multi-line data can be easily represented.
+		+ Data is stored in nested key-value pairs, providing hierarchical data.
+		+ As a standard it is easy to reuse existing code to parse the XML files.
+		+ It is somewhat readable for human beings.
+
+	I've decided to write my own format that requires a lot less typing to enter in by hand.
+	Here's an example settings file using my format:
+
+		; I/O Threads variables:
+		IOThreads 8
+
+			; The buffer count represents the number of buffers for worker threads
+			BufferCount 1000
+
+			; Maximum CPU time in percentage
+			MaxCPUTime 80
+
+	Inside of my application I can do:
+
+		int buffer_count = Settings::ref()->getInt("IOThreads.BufferCount");
+
+	My settings file format has the following deficiencies:
+
+		- Nothing like XSD to validate the file format.
+		- Multi-line data cannot be represented.
+		- Not a standard so not a lot of existing code to parse the format.
+
+	However, XSD validation isn't so important for a lot of applications, and
+	I don't intend to use this format to store website content or whatnot.
+	So the disadvantages of this custom format are outweighed by the benefits:
+
+		+ Faster parsing / shorter load times.
+		+ Easier to write by hand.
+		+ Cleaner and easier to read with just a text editor.
+
+	My settings file wrapper will maintain existing comments and capitalization in the file.
+	It makes a copy of the file data during reading so that if changes need to be made,
+	most of the original file is kept intact.  Key-value pairs are kept in a custom hash table
+	using closed (chained) addressing for speedy lookup, and most algorithms used are zero-copy.
+
+	The file format has been made generic enough for reuse for applications other than settings.
+*/
+
+namespace cat {
+
+namespace ragdoll {
+
+
+static const int MAX_CHARS = 256;
+
+
+//// ragdoll::KeyInput
+
+class CAT_EXPORT KeyInput
+{
+	u32 _hash;
+	const char *_key;
+	int _len;
+
+public:
+	KeyInput(const char *key);
+	KeyInput(const char *key, int len);
+
+	CAT_INLINE u32 Hash() const { return _hash; }
+	CAT_INLINE const char *Key() const { return _key; }
+	CAT_INLINE int Length() const { return _len; }
+};
+
+
+//// ragdoll::HashKey
+
+class CAT_EXPORT HashKey
+{
+protected:
+	NulTermFixedStr<MAX_CHARS> _key;
+	int _len;
+	u32 _hash;
+
+public:
+	HashKey(const KeyInput &key);
+
+	//CAT_INLINE virtual ~HashKey() {}
+
+	CAT_INLINE u32 Hash() { return _hash; }
+
+	CAT_INLINE bool operator==(const KeyInput &key)
+	{
+		return _hash == key.Hash() &&
+			   _len == key.Length() &&
+			   _key.CaseCompare(key.Key(), key.Length());
+	}
+};
+
+
+//// ragdoll::HashValue
+
+class CAT_EXPORT HashValue
+{
+protected:
+	NulTermFixedStr<MAX_CHARS> _value;
+
+public:
+	CAT_INLINE HashValue() {}
+	HashValue(const char *key, int len);
+
+	//CAT_INLINE virtual ~HashValue() {}
+
+	CAT_INLINE int GetValueInt() { return atoi(_value); }
+
+	CAT_INLINE const char *GetValueStr() { return _value; }
+
+	CAT_INLINE void SetValueRangeStr(const char *value, int len)
+	{
+		_value.SetFromRangeString(value, len);
+	}
+
+	CAT_INLINE void SetValueStr(const char *value)
+	{
+		_value.SetFromNulTerminatedString(value);
+	}
+
+	CAT_INLINE void SetValueInt(int ivalue)
+	{
+		_value.SetFromInteger(ivalue);
+	}
+};
+
+
+//// ragdoll::HashItem
+
+class CAT_EXPORT HashItem : public HashKey, public HashValue, public SListItem
+{
+	friend class HashTable;
+
+public:
+	HashItem(const KeyInput &key);
+	HashItem(const KeyInput &key, const char *value, int value_len);
+
+	//CAT_INLINE virtual ~HashItem() {}
+};
+
+
+//// ragdoll::HashTable
+
+class CAT_EXPORT HashTable
+{
+	friend class Iterator;
+
+	CAT_NO_COPY(HashTable);
+
+	static const u32 PREALLOC = 16;
+	static const u32 GROW_THRESH = 2;
+	static const u32 GROW_RATE = 2;
+
+	u32 _allocated, _used;
+	SList *_buckets;
+	typedef SList::Iterator<HashItem> iter;
+
+	bool Grow();
+
+public:
+	HashTable();
+	~HashTable();
+
+	HashItem *Lookup(const KeyInput &key); // Returns 0 if key not found
+	HashItem *Create(const KeyInput &key); // Creates if it does not exist yet
+
+	// Iterator
+	class CAT_EXPORT Iterator
+	{
+		u32 _remaining;
+		SList *_bucket;
+		iter _ii;
+
+		void IterateNext();
+
+	public:
+		Iterator(HashTable &head);
+
+		CAT_INLINE operator HashItem *()
+		{
+			return _ii;
+		}
+
+		CAT_INLINE HashItem *operator->()
+		{
+			return _ii;
+		}
+
+		CAT_INLINE Iterator &operator++() // pre-increment
+		{
+			IterateNext();
+			return *this;
+		}
+
+		CAT_INLINE Iterator &operator++(int) // post-increment
+		{
+			return ++*this;
+		}
+	};
+};
+
+
+//// ragdoll::Parser
+
+class CAT_EXPORT Parser
+{
+	CAT_NO_COPY(Parser);
+
+	static const int MAX_LINE_SIZE = 2048; // Maximum number of bytes per line in the file
+	static const int MAX_TAB_RECURSION_DEPTH = 16; // Maximum number of layers in a key
+	static const int MAX_FILE_SIZE = 4000000; // Maximum number of bytes in file allowed
+
+	// File data
+	u8 *_file_data;
+	u32 _file_offset, _file_size;
+
+	// Parser data
+	char _line[MAX_LINE_SIZE];
+	char _root_key[MAX_CHARS+1];
+	char *_first, *_second;
+	int _first_len, _second_len, _depth;
+
+	// Output data
+	bool _store_offsets;
+	HashTable *_table;
+
+protected:
+	bool NextLine();
+	int ReadTokens(int root_key_len, int root_depth);
+
+public:
+	// Do not pass in the file data or file size pointers if the file is the override file
+	bool Read(const char *file_path, HashTable *output_table, u8 **file_data = 0, u32 *file_size = 0);
+};
+
+
+//// ragdoll::RagdollFile
+
+class CAT_EXPORT RagdollFile
+{
+	HashTable _table;
+
+	bool _readSettings;	// Flag set when settings have been read from disk
+	bool _modified;		// Flag set when settings have been modified since last write
+
+	std::string _settings_path;
+	u8 *_file_data;		// Pointer to settings file data in memory
+	u32 _file_size;		// Number of bytes in settings file
+
+public:
+	RagdollFile();
+	~RagdollFile();
+
+	bool Read(const char *file_path);
+	bool Override(const char *file_path);
+
+	CAT_INLINE HashTable *GetTable() { return &_table; }
+};
+
+
+} // namespace cat
+
+#endif // CAT_RAGDOLL_FILE_HPP
---
+++ trunk/src/io/RagdollFile.cpp
@@ -0,0 +1,568 @@
+/*
+	Copyright (c) 2009-2011 Christopher A. Taylor.  All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+
+	* Redistributions of source code must retain the above copyright notice,
+	  this list of conditions and the following disclaimer.
+	* Redistributions in binary form must reproduce the above copyright notice,
+	  this list of conditions and the following disclaimer in the documentation
+	  and/or other materials provided with the distribution.
+	* Neither the name of LibCat nor the names of its contributors may be used
+	  to endorse or promote products derived from this software without
+	  specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+	ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+	LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+	CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+	POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <cat/io/RagdollFile.hpp>
+#include <cat/parse/BufferTok.hpp>
+#include <cat/io/Logging.hpp>
+#include <cat/io/MappedFile.hpp>
+#include <cat/hash/Murmur.hpp>
+#include <fstream>
+#include <cstring>
+#include <cstdlib>
+using namespace cat;
+using namespace std;
+using namespace ragdoll;
+
+
+// Calculate key hash
+static u32 GetKeyHash(const char *key, int len)
+{
+	char lcase[MAX_CHARS];
+
+	if (len > MAX_CHARS)
+		len = MAX_CHARS;
+
+	// Copy key to temporary buffer all lower-case
+	for (int ii = 0; ii < len; ++ii)
+	{
+		char ch = key[ii];
+
+		if (ch >= 'A' && ch <= 'Z')
+			ch += 'a' - 'Z';
+
+		lcase[ii] = ch;
+	}
+
+	// Calculate hash from lower-case version
+	return MurmurHash(lcase, len).Get32();
+}
+
+
+//// ragdoll::KeyInput
+
+KeyInput::KeyInput(const char *key)
+{
+	int len = (int)strlen(key);
+
+	_hash = GetKeyHash(key, len);
+	_key = key;
+	_len = len;
+}
+
+KeyInput::KeyInput(const char *key, int len)
+{
+	_hash = GetKeyHash(key, len);
+	_key = key;
+	_len = len;
+}
+
+
+//// ragdoll::HashKey
+
+HashKey::HashKey(const KeyInput &key)
+{
+	_key.SetFromRangeString(key.Key(), key.Length());
+	_hash = key.Hash();
+	_len = key.Length();
+}
+
+
+//// ragdoll::HashValue
+
+HashValue::HashValue(const char *value, int len)
+{
+	_value.SetFromRangeString(value, len);
+}
+
+
+//// ragdoll::HashItem
+
+HashItem::HashItem(const KeyInput &key)
+	: HashKey(key)
+{
+}
+
+HashItem::HashItem(const KeyInput &key, const char *value, int value_len)
+	: HashKey(key), HashValue(value, value_len)
+{
+}
+
+
+//// ragdoll::HashTable
+
+bool HashTable::Grow()
+{
+	// Calculate growth rate
+	u32 old_size = _allocated;
+	u32 new_size = old_size * GROW_RATE;
+	if (new_size < PREALLOC) new_size = PREALLOC;
+
+	CAT_INANE("HashTable") << "Growing to " << new_size << " buckets";
+
+	// Allocate larger bucket array
+	SList *new_buckets = new SList[new_size];
+	if (!new_buckets) return false;
+
+	// For each bucket,
+	u32 mask = new_size - 1;
+	for (u32 jj = 0; jj < old_size; ++jj)
+	{
+		// For each bucket item,
+		for (iter ii = _buckets[jj]; ii; ++ii)
+		{
+			new_buckets[ii->Hash() & mask].PushFront(ii);
+		}
+	}
+
+	// Free old array
+	if (_buckets) delete []_buckets;
+
+	_buckets = new_buckets;
+	_allocated = new_size;
+
+	return true;
+}
+
+HashTable::HashTable()
+{
+	_buckets = 0;
+	_allocated = 0;
+	_used = 0;
+}
+
+HashTable::~HashTable()
+{
+	// If any buckets are allocated,
+	if (_buckets)
+	{
+		// For each allocated bucket,
+		for (u32 ii = 0; ii < _allocated; ++ii)
+		{
+			SList &bucket = _buckets[ii];
+
+			// If bucket is not empty,
+			if (!bucket.empty())
+			{
+				// For each item in the bucket,
+				for (iter ii = bucket; ii; ++ii)
+				{
+					// Free item
+					delete ii;
+				}
+			}
+		}
+
+		// Free the array
+		delete []_buckets;
+	}
+}
+
+HashItem *HashTable::Lookup(const KeyInput &key)
+{
+	// If nothing allocated,
+	if (!_allocated) return 0;
+
+	// Search used table indices after hash
+	u32 ii = key.Hash() & (_allocated - 1);
+
+	// For each item in the selected bucket,
+	for (iter jj = _buckets[ii]; jj; ++jj)
+	{
+		// If the key matches,
+		if (*jj == key)
+		{
+			// Found it!
+			return jj;
+		}
+	}
+
+	return 0;
+}
+
+HashItem *HashTable::Create(const KeyInput &key)
+{
+	// Check if it exists already
+	HashItem *item = Lookup(key);
+	if (item) return item;
+
+	// If first allocation fails,
+	if (!_buckets && !Grow()) return 0;
+
+	// If cannot create an item,
+	item = new HashItem(key);
+	if (!item) return 0;
+
+	// If time to grow,
+	if (_used * GROW_THRESH >= _allocated)
+	{
+		// If grow fails,
+		if (!Grow())
+		{
+			delete item;
+			return 0;
+		}
+	}
+
+	// Insert in bucket corresponding to hash low bits
+	u32 bucket_index = key.Hash() & (_allocated - 1);
+	_buckets[bucket_index].PushFront(item);
+
+	// Increment used count to keep track of when to grow
+	++_used;
+
+	return item;
+}
+
+
+//// ragdoll::HashTable::Iterator
+
+void HashTable::Iterator::IterateNext()
+{
+	if (_ii)
+	{
+		++_ii;
+
+		if (_ii) return;
+	}
+
+	while (_remaining)
+	{
+		--_remaining;
+		++_bucket;
+
+		_ii = *_bucket;
+
+		if (_ii) return;
+	}
+}
+
+HashTable::Iterator::Iterator(HashTable &head)
+{
+	_remaining = head._allocated;
+	_bucket = head._buckets;
+	_ii = *_bucket;
+
+	if (!_ii)
+	{
+		IterateNext();
+	}
+}
+
+
+//// ragdoll::Parser
+
+bool Parser::NextLine()
+{
+	int count;
+
+	// Set _first_len to zero by default to indicate no data
+	_first_len = 0;
+
+	// TODO: Rewrite for zero-copy here.  Do EOL/EOF processing inline in this function
+
+	// For each line in the file,
+	if ((count = readLine(_line, (int)sizeof(_line))) < 0)
+		return false;
+
+	// Ignore blank lines (fairly common)
+	if (count == 0) return true;
+
+	// Count the number of tabs and spaces at the front
+	char *first = _line;
+	int tab_count = 0, space_count = 0;
+
+	// While EOL not encountered,
+	char ch;
+	while ((ch = *first))
+	{
+		if (ch == ' ')
+			++space_count;
+		else if (ch == '\t')
+			++tab_count;
+		else
+			break;
+
+		++first;
+	}
+
+	// If EOL found or non-data line, skip this line
+	if (!ch || !IsAlpha(ch)) return true;
+
+	/*
+		Calculate depth from tab and space count
+
+		Round up front 2 spaces to an extra tab in case just
+		the last tab is replaced by spaces and the tab stops
+		are set at 2 characters (attempting to be clever about it)
+	*/
+	int depth = tab_count + (space_count + 2) / 4;
+	if (depth > MAX_TAB_RECURSION_DEPTH) depth = MAX_TAB_RECURSION_DEPTH;
+	_depth = depth;
+
+	// Find the start of whitespace after first token
+	char *second = first + 1;
+
+	while ((ch = *second))
+	{
+		if (ch == ' ' || ch == '\t')
+			break;
+
+		++second;
+	}
+
+	// Store first token
+	_first_len = (int)(second - first);
+	_first = first;
+
+	// If a second token is possible,
+	// NOTE: Second token is left pointing at an empty string here if no whitespace was found
+	int second_len = 0;
+
+	if (ch)
+	{
+		// Terminate the first token
+		*second++ = '\0';
+
+		// Search for end of whitespace between tokens
+		while ((ch = *second))
+		{
+			if (ch == ' ' || ch == '\t')
+				++second;
+			else
+				break;
+		}
+
+		// If second token exists,
+		if (ch)
+		{
+			// Search for end of whitespace between tokens
+			char *end = second + 1;
+
+			// For each character until the end of the line,
+			while ((ch = *end))
+			{
+				// On the first whitespace character encountered,
+				if (ch == ' ' || ch == '\t')
+				{
+					// Terminate the second token and ignore the rest
+					*end = '\0';
+					break;
+				}
+			}
+
+			// Get length of first token
+			second_len = (int)(end - second);
+		}
+	}
+
+	// First and second tokens, their lengths, and the new depth are determined.
+	// The second token may be an empty string but is always a valid string.
+	_second_len = second_len;
+	_second = second;
+
+	return true;
+}
+
+int Parser::ReadTokens(int root_key_len, int root_depth)
+{
+	int eof = 0;
+
+	do
+	{
+		// If there is not enough space to append the first token to the end of the root key,
+		if (root_key_len + 1 + parsed_line.first_len > MAX_CHARS)
+		{
+			// Signal EOF here to avoid mis-attributing keys
+			CAT_WARN("Settings") << "Long line caused settings processing to abort early";
+			return 0;
+		}
+
+		// Append first token to root key
+		int key_len = root_key_len + parsed_line.first_len;
+		char *write_key = root_key + root_key_len;
+
+		if (root_key_len > 0)
+		{
+			root_key[root_key_len] = '.';
+			++write_key;
+			++key_len;
+		}
+
+		memcpy(write_key, parsed_line.first, parsed_line.first_len);
+		write_key[parsed_line.first_len] = '\0';
+
+		// If second token is set,
+		if (parsed_line.second_len)
+		{
+			// Add this path to the hash table
+			HashItem *item = _table.Create(KeyInput(root_key, key_len));
+			if (item) item->SetValueRangeStr(parsed_line.second, parsed_line.second_len);
+		}
+
+		int depth = parsed_line.depth;
+
+		// For each line until EOF,
+		while (readLine(sfile, parsed_line))
+		{
+			// Skip blank lines
+			if (parsed_line.first_len == 0) continue;
+
+			// If new line depth is at or beneath the root,
+			if (root_depth >= parsed_line.depth)
+				eof = 1; // Pass it back to the root to handle
+			// If new line is a child of current depth,
+			else if (depth < parsed_line.depth)
+			{
+				// Otherwise the new line depth is deeper, so recurse and add onto the key
+				eof = readTokens(sfile, parsed_line, root_key, key_len, depth);
+
+				// If not EOF,
+				if (eof != 0)
+				{
+					// If new line depth is at the same level as current token,
+					if (root_depth < parsed_line.depth)
+						eof = 2; // Repeat whole routine again at this depth
+					else
+						eof = 1; // Pass it back to the root to handle
+				}
+			}
+			else // New line depth is at about the same level as current
+				eof = 2; // Repeat whole routine again at this depth
+
+			break;
+		}
+
+		// Remove appended token
+		root_key[root_key_len] = '\0';
+	} while (eof == 2);
+
+	return eof; // EOF
+}
+
+bool Parser::Read(const char *file_path, HashTable *output_table, u8 **file_data, u32 *file_size)
+{
+	CAT_DEBUG_ENFORCE(file_path && output_table);
+
+	// Open the file
+	MappedFile file;
+	if (!file.Open(file_path))
+	{
+		CAT_INFO("Parser") << "Unable to open " << file_path;
+		return false;
+	}
+
+	// Ensure file is not too large
+	u64 file_length = file.GetLength();
+	if (file_length > MAX_SETTINGS_FILE_SIZE)
+	{
+		CAT_WARN("Parser") << "Size too large for " << file_path;
+		return false;
+	}
+
+	// Ensure file is not empty
+	u32 nominal_length = (u32)file_length;
+	if (nominal_length <= 0)
+	{
+		CAT_INFO("Parser") << "Ignoring empty file " << file_path;
+		return false;
+	}
+
+	// Open a view of the file
+	MappedView view;
+	if (!view.Open(&file))
+	{
+		CAT_WARN("Parser") << "Unable to open view of " << file_path;
+		return false;
+	}
+
+	// Map a view of the entire file
+	if (!view.MapView(0, nominal_length))
+	{
+		CAT_WARN("Parser") << "Unable to map view of " << file_path;
+		return false;
+	}
+
+	// Initialize parser
+	_file_data = view.GetFront();
+	_file_offset = 0;
+	_file_size = nominal_length;
+	_root_key[0] = '\0';
+	_store_offsets = false;
+
+	// If file data will be copied out,
+	if (file_data && file_size)
+	{
+		u8 *copy = new u8[nominal_length];
+		if (!copy)
+		{
+			CAT_WARN("Parser") << "Out of memory allocating file buffer of bytes = " << nominal_length;
+			return false;
+		}
+
+		memcpy(copy, _file_data, nominal_length);
+
+		*file_data = copy;
+		*file_size = nominal_length;
+		_store_offsets = true;
+	}
+
+	// Kick off the parsing
+	if (!ReadLine())
+		return false;
+
+	// Bump tokens back to the next level while not EOF
+	while (1 == ReadTokens(0, 0));
+}
+
+
+//// ragdoll::RagdollFile
+
+RagdollFile::RagdollFile()
+{
+	_readSettings = false;
+	_modified = false;
+
+	_file_data = 0;
+	_file_size = 0;
+}
+
+RagdollFile::~RagdollFile()
+{
+	if (_file_data)
+		delete []_file_data;
+}
+
+bool RagdollFile::Read(const char *file_path)
+{
+
+}
+
+bool RagdollFile::Override(const char *file_path)
+{
+
+}
