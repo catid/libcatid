898057fc39851316572177197906c15eddd58bf2
mrcatid@gmail.com <mrcatid@gmail.com@204b7394-75af-11de-9510-cf8a13bd668e>
2012-05-03 02:45:33 +0000

    More work on file transfer

--- include/cat/iocp/AsyncFile.hpp
+++ include/cat/iocp/AsyncFile.hpp
@@ -1,5 +1,5 @@
 /*
-	Copyright (c) 2009-2011 Christopher A. Taylor.  All rights reserved.
+	Copyright (c) 2009-2012 Christopher A. Taylor.  All rights reserved.
 
 	Redistribution and use in source and binary forms, with or without
 	modification, are permitted provided that the following conditions are met:
@@ -60,7 +60,10 @@ enum AsyncFileFlags
 	ASYNCFILE_SEQUENTIAL = 8,
 
 	// Only a good idea for infrequently accessed data or in combination with manual memory caching
-	ASYNCFILE_NOBUFFER = 16
+	ASYNCFILE_NOBUFFER = 16,
+
+	// Truncate the file if it already exists (only makes a difference with writing)
+	ASYNCFILE_TRUNC = 32,
 };
 
 
